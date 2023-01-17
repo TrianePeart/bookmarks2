cfde974a140719e5dc92d7fa6736e7ac1ad5d141
Carlos Godoy <carlosgodoy@pursuit.org>
2023-01-17 15:26:34 -0500

    express/sql seed & read

---
+++ app.js
@@ -0,0 +1,28 @@
+// IMPORTS
+const cors = require("cors");
+const express = require("express");
+const bookmarksController = require("./controllers/bookmarkController.js");
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+// Bookmarks ROUTES
+app.use("/bookmarks", bookmarksController);
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to Bookmarks App");
+});
+
+
+
+// 404 PAGE
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
+
+// EXPORT
+module.exports = app;
